d7115acfa9c96e7fdbfd61b1751e4e5923e814de
Onni Rautanen <oizone@oizone.net>
2021-03-21 22:10:47 +0200

    update

--- create-hosts.py
+++ create-hosts.py
@@ -7,12 +7,12 @@ ws=wb["Hosts"]
 iso_folder="/iso/"
 http_folder="/var/www/httpboot/"
 table=[]
+deployments=[]
 
 for i in ws.iter_rows(min_row=3):
-    values=[i[0].value,i[1].value]
     str_hostaname=i[0].value
     str_nic=i[1].value
-    str_ips=i[2].value
+    str_ip=i[2].value
     str_netmask=i[3].value
     str_gateway=i[4].value
     str_dns=i[5].value
@@ -29,7 +29,11 @@ for i in ws.iter_rows(min_row=3):
     str_esxi=i[16].value
     str_deployment=i[17].value
 
+    values=[str_hostname,str_ip,str_netmask,str_gateway,str_dns,str_vlan,str_idrac_ip,str_domain,str_deployment]
     table+=values
+    if not str_deployment in deployments:
+        deployments+=str_deployment
+
     ks='ks={}/{}/ks.cfg'.format(ws['B1'].value,str_hostaname)
     ks_folder='{}{}'.format(http_folder,str_hostaname)
     #if not os.path.exists(str_hostaname):
@@ -40,42 +44,42 @@ for i in ws.iter_rows(min_row=3):
 
     output.write('vmaccepteula\n')
  
-    output.write('rootpw {}\n'.format(i[12].value))
+    output.write('rootpw {}\n'.format(str_password))
     output.write('clearpart --alldrives --overwritevmfs\n')
 
-    output.write('install {} --overwritevmfs --novmfsondisk\n'.format(i[8].value))
+    output.write('install {} --overwritevmfs --novmfsondisk\n'.format(str_disk_sel))
     output.write('keyboard Finnish\n')
 
-    output.write("network --bootproto=static --device={} --ip={} --netmask={} --gateway={} --nameserver={} --hostname={} --vlanid={} --addvmportgroup=1\n".format(i[1].value,i[2].value,i[3].value,i[4].value,i[5].value,str_hostaname,int(i[6].value)))
+    output.write("network --bootproto=static --device={} --ip={} --netmask={} --gateway={} --nameserver={} --hostname={} --vlanid={} --addvmportgroup=1\n".format(str_nic,str_ip,str_netmask,str_gateway,str_dns,str_hostaname,int(str_vlan)))
 
     output.write('reboot --noeject\n')
 
     output.write('%firstboot --interpreter=busybox\n')
-    if i[9].value != None:
-        capacitydisks=i[9].value.split(',')
+    if str_cap_disk != None:
+        capacitydisks=str_cap_disk.split(',')
         for disk in capacitydisks:
             if disk!='':
                 output.write('esxcli vsan storage tag add -t capacityFlash -d `esxcli storage core device list|grep -B 1 "Display Name:.*{}"|head -n 1`\n'.format(disk))
 
-    output.write('esxcli network ip dns search add --domain={}\n'.format(i[11].value))
+    output.write('esxcli network ip dns search add --domain={}\n'.format(str_domain))
 
-    output.write('esxcli network vswitch standard portgroup set -v {} -p "VM Network"\n'.format(int(i[6].value)))
+    output.write('esxcli network vswitch standard portgroup set -v {} -p "VM Network"\n'.format(int(str_vlan)))
 
     output.write('vim-cmd hostsvc/enable_ssh\n')
 
-    ntps=i[10].value.split(',')
+    ntps=str_ntp.split(',')
     for ntp in ntps:
         if ntp!='':
             output.write('echo "server {}" >> /etc/ntp.conf\n'.format(ntp))
 
 
     output.write('/sbin/chkconfig ntpd on\n')
-    output.write('esxcli system settings advanced set -o /UserVars/HostClientCEIPOptIn -i {}\n'.format(i[13].value))
-    if (i[14].value == "yes"):
+    output.write('esxcli system settings advanced set -o /UserVars/HostClientCEIPOptIn -i {}\n'.format(str_ceip))
+    if (str_tps == "yes"):
         output.write('esxcli system settings advanced set -o "/Mem/AllocGuestLargePage" --int-value 0\n')
         output.write('esxcli system settings advanced set -o "/Mem/ShareForceSalting" --int-value 0\n')
-    if (i[15].value == "yes"):
-        output.write('esxcli vsan network ipv4 add -i {}\n'.format(i[1].value))
+    if (str_vsan == "yes"):
+        output.write('esxcli vsan network ipv4 add -i {}\n'.format(str_nic))
         output.write('esxcli vsan cluster new\n')
         output.write('esxcli vsan policy setdefault -c cluster -p "((\"hostFailuresToTolerate\" i0) (\"forceProvisioning\" i1))"\n')
         output.write('esxcli vsan policy setdefault -c vdisk -p "((\"hostFailuresToTolerate\" i0) (\"forceProvisioning\" i1))")"\n')
@@ -85,23 +89,23 @@ for i in ws.iter_rows(min_row=3):
     output.write('reboot\n')
     output.close()
     
-    if os.path.isfile("{}{}/boot.cfg".format(iso_folder,i[16].value)):
-        bootcfg=open("{}{}/boot.cfg".format(iso_folder,i[16].value),"r").read()
-    elif os.path.isfile("{}{}/BOOT.CFG".format(iso_folder,i[16].value)):
-        bootcfg=open("{}{}/BOOT.CFG".format(iso_folder,i[16].value),"r").read()
+    if os.path.isfile("{}{}/boot.cfg".format(iso_folder,str_esxi)):
+        bootcfg=open("{}{}/boot.cfg".format(iso_folder,str_esxi),"r").read()
+    elif os.path.isfile("{}{}/BOOT.CFG".format(iso_folder,str_esxi)):
+        bootcfg=open("{}{}/BOOT.CFG".format(iso_folder,str_esxi),"r").read()
     else:
-        os.system('7z x -y -o{}{} {}{}.iso'.format(iso_folder,i[16].value,iso_folder,i[16].value))
+        os.system('7z x -y -o{}{} {}{}.iso'.format(iso_folder,str_esxi,iso_folder,str_esxi))
         os.system('find {} -type d -exec chmod o+rx {} \;'.format(iso_folder,'{}'))
-        os.system("find {}{} -mindepth 3 -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;".format(iso_folder,i[16].value,'{}'))
-        os.system("find {}{} -mindepth 2 -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;".format(iso_folder,i[16].value,'{}'))
-        os.system("find {}{} -mindepth 1 -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;".format(iso_folder,i[16].value,'{}'))
-        bootcfg=open("{}{}/boot.cfg".format(iso_folder,i[16].value),"r").read()
+        os.system("find {}{} -mindepth 3 -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;".format(iso_folder,str_esxi,'{}'))
+        os.system("find {}{} -mindepth 2 -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;".format(iso_folder,str_esxi,'{}'))
+        os.system("find {}{} -mindepth 1 -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;".format(iso_folder,str_esxi,'{}'))
+        bootcfg=open("{}{}/boot.cfg".format(iso_folder,str_esxi),"r").read()
 
     
     boot=open("{}/boot.cfg".format(ks_folder),"w+")
     newboot=re.sub("/","",bootcfg,flags=re.M)
     newboot=re.sub(r'title=[^\n]*','title=Loading ESXi installer (https://github.com/oizone/esxihttp)',newboot,flags=re.M)
-    newboot=re.sub(r'prefix=[^\n]*','prefix={}/{}/'.format(ws['B1'].value,i[16].value),newboot,flags=re.M)
+    newboot=re.sub(r'prefix=[^\n]*','prefix={}/{}/'.format(ws['B1'].value,str_esxi),newboot,flags=re.M)
     newboot=re.sub(r'kernelopt=[^\n]*','kernelopt={}'.format(ks),newboot,flags=re.M)
     boot.write(newboot)
     boot.close()
@@ -110,14 +114,15 @@ for i in ws.iter_rows(min_row=3):
     if os.path.islink('{}/mboot.efi'.format(ks_folder)):
         os.remove('{}/mboot.efi'.format(ks_folder))
 
-    if not os.path.exists('{}{}'.format(http_folder,i[16].value)):
-        os.symlink('{}{}'.format(iso_folder,i[16].value),'{}{}'.format(http_folder,i[16].value))
+    if not os.path.exists('{}{}'.format(http_folder,str_esxi)):
+        os.symlink('{}{}'.format(iso_folder,str_esxi),'{}{}'.format(http_folder,str_esxi))
 
-    if os.path.isfile("{}{}/efi/boot/bootx64.efi".format(iso_folder,i[16].value)):
-        os.symlink('{}{}/efi/boot/bootx64.efi'.format(iso_folder,i[16].value),'{}/mboot.efi'.format(ks_folder))
+    if os.path.isfile("{}{}/efi/boot/bootx64.efi".format(iso_folder,str_esxi)):
+        os.symlink('{}{}/efi/boot/bootx64.efi'.format(iso_folder,str_esxi),'{}/mboot.efi'.format(ks_folder))
     else:
-        os.symlink('{}{}/EFI/BOOT/BOOTX64.EFI'.format(iso_folder,i[16].value),'{}/mboot.efi'.format(ks_folder))
+        os.symlink('{}{}/EFI/BOOT/BOOTX64.EFI'.format(iso_folder,str_esxi),'{}/mboot.efi'.format(ks_folder))
 
     
 
 print(table)
+print(deployments)
