88ca72dcc296da1f8599005347d3c47ab2fd2137
Onni Rautanen <oizone@oizone.net>
2021-03-21 12:19:21 +0200

    1st releasse test

--- extract-iso.sh
+++ extract-iso.sh
@@ -5,7 +5,7 @@ MOUNTBASE="/httpboot/iso/"
 
 for iso in `find ${ISOPATH} -type f -name "*.iso"`
 do
-    ISONAME=`echo ${iso}|cut -d"/" -f 3`
+    ISONAME=`echo ${iso}|cut -d"/" -f 3|cut -d"." -f 1`
     mkdir -p "${MOUNTBASE}${ISONAME}"
-    mount -o loop ${iso} "${MOUNTBASE}${ISONAME}"
+    7z x -y -o"${MOUNTBASE}${ISONAME}" ${iso}
 done
