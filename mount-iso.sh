89d53b862954d22f932dc131f16d8668006afce3
Onni Rautanen <oizone@oizone.net>
2021-03-21 11:04:32 +0200

    add -p to mount

--- mount-iso.sh
+++ mount-iso.sh
@@ -6,6 +6,6 @@ MOUNTBASE="/httpboot/iso/"
 for iso in `find ${ISOPATH} -type f -name "*.iso"`
 do
     ISONAME=`echo ${iso}|cut -d"/" -f 3`
-    mkdir "${MOUNTBASE}${ISONAME}"
+    mkdir -p "${MOUNTBASE}${ISONAME}"
     mount -o loop ${iso} "${MOUNTBASE}${ISONAME}"
 done
